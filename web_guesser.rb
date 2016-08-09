210a6a19b1a96faf99aafdf5dc91dcc9e44fe1eb
Daniel Broadbent <dbroadbent85@gmail.com>
2016-08-09 09:31:22 -0600

    color background guess indicator

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,7 +7,8 @@ get '/' do
   cheat = params["cheat"]
   cheat_enabled = cheat_mode(cheat, number)
   message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :message => message, :cheat_enabled=> cheat_enabled}
+  color = check_color(guess, number)
+  erb :index, :locals => {:number => number, :message => message, :cheat_enabled=> cheat_enabled, :color => color}
 end
 
 def cheat_mode(cheat, number)
@@ -27,5 +28,19 @@ def check_guess(guess, number)
     "Your guess was too low..."
   else guess.to_i == number
     "Your guess was right on! </br> The secret number is #{number}"
+  end
+end
+
+def check_color(guess, number)
+  if guess.to_i - 5 > number
+    "red"
+  elsif guess.to_i > number
+    "yellow"
+  elsif guess.to_i + 5 < number
+    "red"
+  elsif guess.to_i < number
+    "yellow"
+  else guess.to_i == number
+    "green"
   end
 end
