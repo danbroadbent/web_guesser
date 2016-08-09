71250540d3d4cb5f4c8c28e488c8fd2bc11275e1
Daniel Broadbent <dbroadbent85@gmail.com>
2016-08-09 09:24:18 -0600

    cheat mode enabled

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,8 +4,16 @@ require 'sinatra/reloader'
 number = rand(100)
 get '/' do
   guess = params["guess"]
+  cheat = params["cheat"]
+  cheat_enabled = cheat_mode(cheat, number)
   message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :message => message}
+  erb :index, :locals => {:number => number, :message => message, :cheat_enabled=> cheat_enabled}
+end
+
+def cheat_mode(cheat, number)
+  if cheat == "true"
+    "Cheat mode enabled, the secret number is #{number} <br />"
+  end
 end
 
 def check_guess(guess, number)
@@ -18,6 +26,6 @@ def check_guess(guess, number)
   elsif guess.to_i < number
     "Your guess was too low..."
   else guess.to_i == number
-    "Your guess was right on! </br> The secrect number is #{number}"
+    "Your guess was right on! </br> The secret number is #{number}"
   end
 end
